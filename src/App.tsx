57a92b939cd333c5c31574261ab82223febbffb0
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2023-06-19 22:39:49 +0100

    chore(refactor): extract code preview and code cell :hammer: :zap:

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { useState } from 'react';
-
-import bundler from './bundler';
-
-import Preview from './components/preview';
-import CodeEditor from './components/code-editor';
+import CodeCell from './components/code-cell';
 
 function App() {
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
-
-  const handleClick = async () => {
-    const output = await bundler(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue='const a = 1;'
-        onChange={(value) => setInput(value)}
-      />
-
-      <div>
-        <button onClick={handleClick}>Submit</button>
-      </div>
-
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 }
