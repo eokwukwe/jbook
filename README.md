4494739a7ebdd22bb0f603c38b440d0872cb27d4
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2023-07-11 02:42:56 +0100

    chore(folder): rename folders to match package names :fire: :hammer:

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# jbook
+# jsjotta
 
 
 > A CLI to launch an interactive development environment for writing and documenting js code.
--- packages/jsjotta-client/src/main.tsx
+++ packages/jsjotta-client/src/main.tsx
@@ -6,7 +6,7 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import App from './App.tsx';
-import store from './store';
+import store from './store/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
